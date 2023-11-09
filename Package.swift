9283a4979386ef284eda8e9c774c94790591777c
Juraj Antas <juraj.antas@gmail.com>
2023-11-09 18:59:16 +0100

    second

--- Package.swift
+++ Package.swift
@@ -29,11 +29,11 @@ let package = Package(
             checksum: "5c97f924fff41d0059ddc85b146c73911c3232a71742b262859213cee99fc702"),
         .binaryTarget(
             name: "GoogleMapsBase",
-            url: "https://github.com/jurajantas/GoogleMapsBase/releases/download/7.4.0/GoogleMapsBase.xcframework.zip",
+            url: "https://github.com/jurajantas/GoogleMaps/releases/download/7.4.0/GoogleMapsBase.xcframework.zip",
             checksum: "3f43aa4c7d70f6fecac33519009c66b478e64fe7304db7dd801488a7819bf3e5"),
         .binaryTarget(
             name: "GoogleMapsCore",
-            url: "https://github.com/jurajantas/GoogleMapsCore/releases/download/7.4.0/GoogleMapsCore.xcframework.zip",
+            url: "https://github.com/jurajantas/GoogleMaps/releases/download/7.4.0/GoogleMapsCore.xcframework.zip",
             checksum: "e374f3a6be40f2e4f91092badd6ed414c43777005eaa141cd8f5b35fea8fd4b2")
 
     ]
